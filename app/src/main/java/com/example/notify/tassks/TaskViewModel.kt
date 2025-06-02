ec358ec00b2804bd21f41ed30ffac3415ede0487
l1kiiiiii <ailikhith2116@gmail.com>
2025-06-02 21:17:41 +0530

    changing into static background

--- app/src/main/java/com/example/notify/tassks/TaskViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.notify.tassks
-
-import androidx.lifecycle.ViewModel
-import com.google.firebase.firestore.FirebaseFirestore
-
-class TaskViewModel: ViewModel(){
-    private val firestore= FirebaseFirestore.getInstance()
-
-}
